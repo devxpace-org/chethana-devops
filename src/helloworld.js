102f347d49da07911b329de885b23dd1ac6d288c
chetanareddy98 <135719546+chetanareddy98@users.noreply.github.com>
2023-06-29 15:26:44 -0400

    Update helloworld.js

--- src/helloworld.js
+++ src/helloworld.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/supriya*', (req, res) => {
-  res.send('This is supriya!')
+app.get('/teju*', (req, res) => {
+  res.send('This is teju!')
 })
 
 app.listen(port, () => {
